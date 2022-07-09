281f3889e4232c893e223c18fb35a8dc5374cf98
ChristianGalindo10 <cygc1@hotmail.com>
2022-07-09 18:17:01 -0500

    axios connection

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,21 @@
 <template>
-  <Tutorial/>
+  <div>
+    <Tutorial />
+    <div style="padding: 10px">
+      <h1>Nuxt.js + Spring Boot</h1>
+      <p>Message from API: {{ ip }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+/**export default {
   name: 'IndexPage'
+}**/
+export default {
+  async asyncData({ app }) {
+    const ip = await app.$axios.$get('http://localhost:8080');
+    return { ip }
+  }
 }
 </script>
