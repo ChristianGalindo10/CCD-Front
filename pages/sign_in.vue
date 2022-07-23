e65a06b48a0b0ba7c054b13be1edd6edd1987808
ChristianGalindo10 <cygc1@hotmail.com>
2022-07-22 21:20:14 -0500

    Sign in restaurant

--- pages/sign_in.vue
+++ pages/sign_in.vue
@@ -79,7 +79,9 @@ export default {
             // Reset our form values
             this.form.email = ''
             this.form.name = ''
-            this.form.food = null
+            this.form.type = null
+            this.form.celular = null
+            this.form.password = null
             this.form.checked = []
             // Trick to reset/clear native browser form validation state
             this.show = false
